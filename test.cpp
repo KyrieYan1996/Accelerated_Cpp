5e7c53b9d1baaf3e74f8837fa124fa2ce9bdc2a0
caulyYan <821314782@qq.com>
2018-12-16 12:53:24 +0800

    first commit

---
+++ test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    //std::cout << "Please input ur name:" << std::endl;
+    std::cout << "Please input ur name:" ;//与第一句的区别在于是否会结束本行！
+    std::string name;
+    std::cin >> name;
+    std::cout << "hello, " << name << "!" << std::endl;
+    const std::string greeting = "hello, " + name + "!";
+    std::cout << "len(greeting):" << greeting.size() << std::endl;
+    const std::string spaces(greeting.size(),' ');
+    const std::string first(greeting.size()+4,'*');
+    const std::string second = "**" + spaces + "**";
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " + greeting + " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
+    return 0;
+}
+/*
+int main()
+{
+    std::cout << "whats ur name?" ;
+    std::string name;
+    std::cin >> name;
+    std::cout << "my name is " + name +"and ur name?";
+    std::cin >> name;
+    std::cout << "and my name is " + name << std::endl;
+    return 0;
+}*/
+
